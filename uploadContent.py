ebbbe8836564b2aeeb47c836d3713a939e43c6fd
Aitor <69603173+aitortxu20@users.noreply.github.com>
2023-10-16 22:57:28 +0200

    Update uploadContent.py

--- uploadContent.py
+++ uploadContent.py
@@ -1,8 +1,4 @@
 from googleapiclient.http import MediaFileUpload
-#from google.auth.transport.requests import Request
-#from google.oauth2.credentials import Credentials
-#from google_auth_oauthlib.flow import InstalledAppFlow
-#from googleapiclient.discovery import build
 from quickstart import FOLDER_ID, FILE_ID
 import shutil, os
 import time
